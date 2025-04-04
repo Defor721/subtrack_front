6dfeb7bad67491a726113ee14d63bc34231b1ac2
Defor721 <92020095+Defor721@users.noreply.github.com>
2025-04-04 18:30:01 +0900

    [Feat] 필요없는 파일 제거

--- src/app/cancel/page.tsx
+++ src/app/cancel/page.tsx
@@ -5,7 +5,10 @@ export default function CancelPage() {
       <p className="text-lg mb-6">
         결제가 취소되었거나 오류가 발생했습니다. 다시 시도해주세요.
       </p>
-      <a href="/plans" className="text-blue-600 hover:underline font-medium">
+      <a
+        href="/plans"
+        className="text-blue-600 hover:underline font-medium  cursor-pointer"
+      >
         요금제 다시 선택하기
       </a>
     </main>
